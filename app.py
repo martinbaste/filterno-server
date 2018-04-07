42e0a9c300f854d7dd793cb2e86a244deebb138d
Martín <martinbaste@gmail.com>
2018-04-07 23:29:34 +0200

    add gelt function

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request;
+from flask import Flask, render_template, request
 from flask_cors import CORS
+from utils.query_GDELT import submit_query
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/analyze')
 def analyze():
-    return '{"text":"' + request.args.get('text') + '"}';
+	text = request.args.get('text')
+	res = submit_query(text.split(' '))
+	return res
