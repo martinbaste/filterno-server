11ce3bd0dc3e30751d8db120b97b13350e07816b
Adrian Zucco <adrigabzu@gmail.com>
2018-04-07 23:30:39 +0200

    Query func HTML for plot

--- utils/query_GDELT.py
+++ utils/query_GDELT.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Author: adrian
 # @Date:   2018-04-07 22:41:21
-# @Last Modified by:   adrian
-# @Last Modified time: 2018-04-07 23:15:18
+# @Last Modified by:   Adrian Zucco
+# @Last Modified time: 2018-04-07 23:29:23
 
 
 import json
 import requests
-import pandas as pd
 import urllib
 
 
@@ -23,12 +22,13 @@ def submit_query(query_words, mode=None, TIMESPAN=None):
 
     if mode != None:
         parameters["mode"] = mode
+        parameters["format"] = "HTML"
     if TIMESPAN != None:
         parameters["TIMESPAN"] = TIMESPAN
 
 
     params = urllib.parse.urlencode(parameters, quote_via=urllib.parse.quote)
     response = requests.get("https://api.gdeltproject.org/api/v2/doc/doc", params=params)
-    response_json = response.json()
+    response_json = response.content
 
     return response_json
