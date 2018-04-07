fbe23e28f89483ca97ce5e56e79b6f37c5d4064e
Martín <martinbaste@gmail.com>
2018-04-07 22:17:19 +0200

    sample frontend

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request;
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/term/<term>')
-def search_term(term):
-    return 'Term ' + term 
+@app.route('/analyze')
+def analyze():
+    return request.args.get('text');
